2ef085c2a51dd907ee26769143d29d767582a9fc
Sam Young <youngsam015@gmail.com>
2018-05-01 11:50:19 -0400

    Updated a bunch of things!

--- logger.py
+++ logger.py
@@ -6,41 +6,52 @@ import json
 
 client = discord.Client()
 
-print("Enter your discord e-mail here:")
-user=input()
-print("Enter your discord password here:")
-password=input()
-print("Enter !logServer in a server you want to log")
+print("Would you like to use an account or bot token?")
+print("1.Account")
+print("2.Bot Token")
+option=input()
+if option=="1":
+    print("Enter your discord e-mail here:")
+    user=input()
+    print("Enter your discord password here:")
+    password=input()
+elif option=="2":
+    print("Enter the bot token:")
+    token=input()
+else:
+    print("Something went wrong.")
 print("---\n---\n---")
 
 
 @client.event
 async def on_ready():
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('------')
-
-@client.event
-async def on_message(message):
-	if message.content.startswith('!logServer') and message.author.name==client.user.name:
-		if not os.path.exists(message.server.name):
-			os.makedirs(message.server.name)
-		for chan in message.server.channels:
-			print("Beginning logging of channel " + chan.name)
-			f=open(message.server.name+"/"+"log-"+chan.name,"w")
-			async for msg in client.logs_from(chan,limit=10000000000,reverse=True):
-				try:
-					f.write(msg.timestamp.strftime("%c")+"     "+msg.author.name + ": " + msg.clean_content+"\n")
-				except:
-					f.write("Something went wrong with this message. ;w;\n")
-				if len(msg.attachments)>0:
-						f.write("     ATTACHMENTS: ")
-						for att in msg.attachments:
-							f.write("        "+att['url']+"\n")
-			print("Finished logging channel " + chan.name)
-			f.close()
-		print("Log complete!")
+    for serv in client.servers:
+        print("Beginning logging of server " + serv.name)
+        for chan in serv.channels:
+            print("Beginning logging of channel " + chan.name)
+            try:
+                os.mkdir(serv.name)
+            except:
+                pass
+            f=open(serv.name+"/"+"log-"+chan.name,"w+")
+            counter=0
+            #total=len(client.logs_from(chan,limit=1000000000,reverse=True))
+            async for msg in client.logs_from(chan,limit=10000000000,reverse=True):
+                counter+=1
+                try:
+                    f.write(msg.timestamp.strftime("%c")+"     "+msg.author.name + ": " + msg.clean_content+"\n")
+                except:
+                    f.write("Something went wrong with this message. ;w;\n")
+                if len(msg.attachments)>0:
+                    f.write("     ATTACHMENTS: ")
+                    for att in msg.attachments:
+                        f.write("        "+att['url']+"\n")
+                #print("Message #" + str(counter) + "out of " + str(total))
+            print("Finished logging channel " + chan.name)
+    f.close()
+    print("Log complete!")
 		
-
-client.run(user,password)
+if option=="1":
+    client.run(user,password)
+elif option=="2":
+    client.run(token)
